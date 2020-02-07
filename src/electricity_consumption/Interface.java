e038b28a04d0eaef49d021b79082aaad8b0d31ec
Taky <e1801588@edu.vamk.fi>
2020-02-07 23:51:23 +0200

    Interface class

---
+++ src/electricity_consumption/Interface.java
@@ -0,0 +1,20 @@
+package electricity_consumption;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Interface {
+	
+}
