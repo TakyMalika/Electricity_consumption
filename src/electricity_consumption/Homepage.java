cb6f2cac8e3a7d65d9a191eaf0642975286901b4
Taky <e1801588@edu.vamk.fi>
2020-02-09 19:37:43 +0200

    Added Homepage

---
+++ src/electricity_consumption/Homepage.java
@@ -0,0 +1,57 @@
+package electricity_consumption;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Homepage extends Application {
+    //start method is main entry point for application. 
+    //Stage is top-level container.
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Electricity Consumption");
+
+        //GridPane for flexible layout of controls in rows and columns
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10); //Horizontal space between columns
+        grid.setVgap(10); //Vertical space between rows
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Button btn1 = new Button("Insert Data");
+        grid.add(btn1, 0, 0);
+        
+        Button btn2 = new Button("Search a date");
+        grid.add(btn2, 0, 1);
+        
+        Button btn3 = new Button("See total bar chart");
+        grid.add(btn3, 0, 2);
+        
+        // Event handling for button
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                Insert_data_display insertdata = new Insert_data_display();
+                insertdata.start(primaryStage);
+            }
+        });
+
+        //Scene is container for all content 
+        
+        Scene scene = new Scene(grid, 500, 475);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
